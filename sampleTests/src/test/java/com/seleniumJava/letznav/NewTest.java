60cae799bbe2d333733ad4a181e0239d56a8d26a
ankur.sh <ankur.sh@ETHYDLP084.excershyd.com>
2018-04-06 15:03:30 +0530

    Selenium Test Added

--- sampleTests/src/test/java/com/seleniumJava/letznav/NewTest.java
+++ sampleTests/src/test/java/com/seleniumJava/letznav/NewTest.java
@@ -2,31 +2,33 @@ package com.seleniumJava.letznav;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
 public class NewTest {
+	WebDriver driver;
 	@Test(description="sample Tests")
-	public void f() {
-		int result = 20;
-		boolean fix = false;
-		if (result % 2 == 0) {
-			fix = true;
-		} else {
-			fix = false;
-		}
-		Assert.assertTrue(fix);
-
+	public void SampleTest() {
+		boolean result = driver.findElement(By.className("_2OJxl5")).isDisplayed();
+		Assert.assertTrue(result);
 	}
-
+	// change the path of Chrome Driver 
 	@BeforeClass
-	public void beforeClass() {
-		System.out.println("Before class letznav");
+	public void beforeClass() throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		Thread.sleep(5000);
+		
 	}
 
 	@AfterClass
 	public void afterClass() {
-		System.out.println("After class letznav");
+		driver.quit();
 	}
 
 }
